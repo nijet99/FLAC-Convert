f1e7c5c95a1f165ec312a03cd0bcc38fedf47660
nijet99 <nijet99@gmail.com>
2009-06-21 22:06:55 +0200

    Put under GPL

--- flacconvert.sh
+++ flacconvert.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+
+#############################################################################
+#                                                                           #
+#    Copyright 2009, nijet99@gmail.com                                      #
+#                                                                           #
+#    This file is part of FLAC-Convert                                      #
+#                                                                           #
+#    Foobar is free software: you can redistribute it and/or modify         #
+#    it under the terms of the GNU General Public License as published by   #
+#    the Free Software Foundation, either version 3 of the License, or      #
+#    (at your option) any later version.                                    #
+#                                                                           #
+#    Foobar is distributed in the hope that it will be useful,              #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+#    GNU General Public License for more details.                           #
+#                                                                           #
+#    You should have received a copy of the GNU General Public License      #
+#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.        #
+#                                                                           #
+#############################################################################
+
+
 basefolder='/home/user/rtorrent/'
 flacfolder=$basefolder'/Downloads'	#where to start looking for .flac files
 dest[1]='What_320/'			#still hardcoded - serves as indication what to do with the files
