4c19fb772498c4d6120c6b18999efaf4b0d929c2
nijet99 <nijet99@gmail.com>
2009-06-23 15:53:53 +0200

    fixed comment spacing

--- flacconvert.sh
+++ flacconvert.sh
@@ -2,29 +2,29 @@
 
 
 #############################################################################
-# #
-# Copyright 2009, nijet99@gmail.com #
-# #
-# This file is part of FLAC-Convert #
-# #
-# Foobar is free software: you can redistribute it and/or modify #
-# it under the terms of the GNU General Public License as published by #
-# the Free Software Foundation, either version 3 of the License, or #
-# (at your option) any later version. #
-# #
-# Foobar is distributed in the hope that it will be useful, #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the #
-# GNU General Public License for more details. #
-# #
-# You should have received a copy of the GNU General Public License #
-# along with FLAC-Convert. If not, see <http://www.gnu.org/licenses/>. #
-# #
+#                                                                           #
+# Copyright 2009, nijet99@gmail.com                                         #
+#                                                                           #
+# This file is part of FLAC-Convert                                         #
+#                                                                           #
+# Foobar is free software: you can redistribute it and/or modify            #
+# it under the terms of the GNU General Public License as published by      #
+# the Free Software Foundation, either version 3 of the License, or         #
+# (at your option) any later version.                                       #
+#                                                                           #
+# Foobar is distributed in the hope that it will be useful,                 #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              #
+# GNU General Public License for more details.                              #
+#                                                                           #
+# You should have received a copy of the GNU General Public License         #
+# along with FLAC-Convert. If not, see <http://www.gnu.org/licenses/>.      #
+#                                                                           #
 #############################################################################
 
 
 ##############################################################################
-# DEFINE VARIABLES #
+#                             DEFINE VARIABLES                               #
 ##############################################################################
 
 # Define announce url
@@ -77,8 +77,8 @@ opt_arr[4]="-q 8"
 
 
 ##############################################################################
-# DEFINE USER FUNCTIONS #
-# do not edit below #
+#                           DEFINE USER FUNCTIONS                            #
+#                             do not edit below                              #
 ##############################################################################
 
 
@@ -190,8 +190,8 @@ function create_torrents
 
 
 ##############################################################################
-# SCRIPT CONTROL #
-# do not edit below #
+#                               SCRIPT CONTROL                               #
+#                             do not edit below                              #
 ##############################################################################
 
 
