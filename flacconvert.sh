31eca2f3712e0c4aa4de2c8e7bc3ed32529c4351
nijet99 <nijet99@gmail.com>
2009-06-23 17:32:01 +0200

    foobar --> FLAC-Convert in licence header

--- flacconvert.sh
+++ flacconvert.sh
@@ -5,9 +5,7 @@
 #                                                                           #
 # Copyright 2009, nijet99@gmail.com                                         #
 #                                                                           #
-# This file is part of FLAC-Convert                                         #
-#                                                                           #
-# Foobar is free software: you can redistribute it and/or modify            #
+# FLAC-Convert is free software: you can redistribute it and/or modify      #
 # it under the terms of the GNU General Public License as published by      #
 # the Free Software Foundation, either version 3 of the License, or         #
 # (at your option) any later version.                                       #
